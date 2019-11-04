bf38baece8933a207f8d8916948f782514d9d68c
Sturla Þorvaldsson <sturla@mavericks.is>
2019-11-04 18:20:02 +0000

    Add project files.

---
+++ App2/App2/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
