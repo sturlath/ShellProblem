e9eb9c8bd45cf9877ff935bc2c0d9dc4a84f0b7e
Sturla Þorvaldsson <sturla@mavericks.is>
2019-11-05 15:41:16 +0000

    "On click artifact bug" when a style is applied to a button on Android.

---
+++ App2/App2/Styles/Themes/WhiteTheme.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App.Styles.Themes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WhiteTheme : ResourceDictionary
+    {
+        public WhiteTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
